621585d7cbc6ba4ff5d70858b42e2dcd20fc1520
kbj184 <gsskbjss@gmail.com>
2025-12-27 23:49:19 +0900

    chore: Add debug logging to MyController for crew info

--- src/main/java/com/secondwind/controller/MyController.java
+++ src/main/java/com/secondwind/controller/MyController.java
@@ -42,13 +42,21 @@ public class MyController {
 
         // Crew Info Logic
         var crewMember = crewMemberRepository.findByUserId(userAuth.getId());
+        System.out.println("DEBUG: Finding crew for user ID " + userAuth.getId());
+
         if (crewMember.isPresent()) {
+            System.out.println("DEBUG: Found crew member record. Crew ID: " + crewMember.get().getCrewId());
             var crew = crewRepository.findById(crewMember.get().getCrewId());
             if (crew.isPresent()) {
+                System.out.println("DEBUG: Found crew details. Name: " + crew.get().getName());
                 userDTO.setCrewId(crew.get().getId());
                 userDTO.setCrewName(crew.get().getName());
                 userDTO.setCrewImage(crew.get().getImageUrl());
+            } else {
+                System.out.println("DEBUG: Crew found but details missing for ID " + crewMember.get().getCrewId());
             }
+        } else {
+            System.out.println("DEBUG: No crew member record found for user.");
         }
 
         return userDTO;
