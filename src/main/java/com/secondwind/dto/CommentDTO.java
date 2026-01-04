b328a15facd856f34eeb48e58b0b7eef21b8c937
kbj184 <gsskbjss@gmail.com>
2026-01-04 14:07:39 +0900

    fix: Add Lombok constructors to CommentDTO for JSON support

--- src/main/java/com/secondwind/dto/CommentDTO.java
+++ src/main/java/com/secondwind/dto/CommentDTO.java
@@ -2,10 +2,14 @@ package com.secondwind.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentDTO {
     private Long id;
     private Long postId;
